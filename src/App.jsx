7b32b0b2c0ffc3f8bb61ccae20c340df7bc0c808
Saara Khan <khansarah0716@gmail.com>
2024-09-22 16:54:38 +0530

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Topbar from './components/TopBar/Topbar';
+import Home from './pages/home/home';
 
 function App() {
   return (
     <>
       <Topbar />
+      <Home />
     </>
   );
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import Header from '../../components/Header/Header';
+import './home.css';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      Home-Page
+    </div>
+  );
+};
+
+export default Home;
