1ce93f305d5ddb8bf2b85a526f0e867964bf92d6
Saara Khan <khansarah0716@gmail.com>
2024-09-24 11:43:20 +0530

    Changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,14 +2,12 @@ import './header.css';
 import img from './img.png';
 const Header = () => {
   return (
-    <div>
-      <div className='header'>
-        <div className='headerTitles'>
-          <div className='headerTitle1'>Your Destination for Insights</div>
-          <div className='headerTitle2'>Realm Blog</div>
-        </div>
-        <img className='headerImg' src={img} alt='' />
+    <div className='header'>
+      <div className='headerTitles'>
+        <div className='headerTitle1'>Your Destination for Insights</div>
+        <div className='headerTitle2'>Realm Blog</div>
       </div>
+      <img className='headerImg' src={img} alt='' />
     </div>
   );
 };
