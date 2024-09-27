cb5dc61240d94a1d219511540bdb3912b484b1fd
Saara Khan <khansarah0716@gmail.com>
2024-09-27 19:09:37 +0530

    Publish page addition

---
+++ src/pages/write/Write.jsx
@@ -0,0 +1,24 @@
+import './write.css';
+const Write = () => {
+  return (
+    <div className='write'>
+      <img src='https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&w=600' className='writeImg' alt='' />
+      <form className='writeForm'>
+        <div className='writeFormGroup'>
+          <label htmlFor='fileInput'>
+            <i className='writeIcon fa-solid fa-plus'></i>
+          </label>
+          <input type='file' id='fileInput' className='inputText' />
+          <input type='text' placeholder='Title' className='writeInput' autoFocus={true} />
+        </div>
+        <div className='writeFormGroup'>
+          <textarea placeholder='Tell your story...' type='text' className='writeInput writeText'></textarea>
+        </div>
+
+        <button className='writeSubmit'>Publish</button>
+      </form>
+    </div>
+  );
+};
+
+export default Write;
