81251bba09f9bb7a64c70ddcf7f7173be0a153ea
Saara Khan <khansarah0716@gmail.com>
2024-09-28 20:32:42 +0530

    responsive added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,15 @@
 import './header.css';
+
 const Header = () => {
-  
   return (
     <div className='header'>
       <div className='headerTitles flex'>
-        <div className='headerTitle1'>Your Destination for Insights</div>
-        <div className='headerTitle2'>Realm Blog!</div>
+        <div className='headerTitle1 animate-title'>Your Destination for Insights</div>
+        <div className='headerTitle2 animate-title'>Realm Blog!</div>
       </div>
-      <p className='flex'>Explore a wide range of captivating blogs through our intuitive platform. Join our community of avid readers today.</p>
-      
+      <p className='flex animate-description'>
+        Explore a wide range of captivating blogs through our intuitive platform. Join our community of avid readers today.
+      </p>
     </div>  
   );
 };
