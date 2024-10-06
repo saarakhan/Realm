902d527c456b3ed5730c49e24db84e9f2da54b5a
Saara Khan <khansarah0716@gmail.com>
2024-10-06 21:17:22 +0530

    mongoose connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const { default: mongoose } = require('mongoose');
+const authRouter = require("./routes/auth");
 const app = express();
-const port = '3000';
 
-app.listen(port, () => {
+dotenv.config();
+app.use(express.json()) // to accept json data
+
+const port = 3000;
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("connection successful");
+    }).catch((err) => {
+        console.log(err);
+    })
+
+app.use("/api/auth", authRouter);   
+app.listen(3000, () => {
     console.log(`backend is running on ${port}`);
-})
+})  
