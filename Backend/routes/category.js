c4f2ef60701762e6d6920a564ca58f9b60f52b5e
Saara Khan <khansarah0716@gmail.com>
2024-10-07 22:18:56 +0530

    GET & POST categories

---
+++ Backend/routes/category.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const Category = require("../models/Category");
+
+// Add a new Cat
+router.post("/", async (req, res) => {
+    const newCat = new Category(req.body);
+    try {
+        const savedCat = await newCat.save();
+        res.status(200).json(savedCat)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// GET ALL CAt
+router.get("/", async (req, res) => {
+    try {
+        const allCat = await Category.find();
+        res.status(200).json(allCat)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+
+
+
+
+module.exports = router;
