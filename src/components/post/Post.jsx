6c3c7ac0758da8e6b202d99aa3495580ceb81ced
Saara Khan <khansarah0716@gmail.com>
2024-10-13 17:20:17 +0530

    images file path change

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,10 +1,11 @@
 import './post.css';
 import { Link } from 'react-router-dom';
 const Post = ({ post }) => {
+  const PF = 'http://localhost:3000/images/';
   return (
     <>
       <div className='post'>
-        {post.photo && <img className='postImg' src={post.photo} alt='' />}
+        {post.photo && <img className='postImg' src={PF + post.photo} alt='' />}
         <div className='postInfo'>
           <div className='postCats'>
             {post.categories.map(cat => {
@@ -15,7 +16,7 @@ const Post = ({ post }) => {
             <span className='postTitle'>{post.title}</span>
           </Link>
           <hr />
-          <div className='postDate'>{new Date(post.createdAt).toDateString}</div>
+          <div className='postDate'>{new Date(post.createdAt).toDateString()}</div>
         </div>
         <p className='postDesc'>{post.desc}</p>
       </div>
